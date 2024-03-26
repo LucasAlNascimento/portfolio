a12167f7b5b3c13da6d026e5c96a004a3c97fc95
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2024-03-26 13:40:50 -0300

    Alterando caminho imagem background

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
 
 <style>
 .background-app {
-  background-image: url('src/assets/backgroundimage.png');
+  background-image: url('src/backgroundimage.png');
   background-size: cover;
   background-position: center;
   height: auto;
