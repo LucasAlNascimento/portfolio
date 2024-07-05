7c8c63f57e347a6227815d0660c7b2b419dffaad
Lucas Alves <147735920+LucasAlNascimento@users.noreply.github.com>
2024-07-04 21:23:38 -0300

    Refatorando About para Melhorar Responsividade Mobile

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,63 +4,38 @@
         <Header />
     </v-container>
 
-    <v-container class="d-flex flex-column flex-md-row ga-16">
-        <v-container class="d-flex flex-column ga-9">
-            <h1 class="text-teal-accent-3 text-uppercase text-h3 font-weight-black">Um pouco Sobre Mim</h1>
-            <p class="text-white text-justify font-weight-medium">
-                Me chamo Lucas, tenho 24 anos e sou graduado em <span class="text-teal-accent-4 font-weight-medium"> Ciência
-                    da Computação</span> pela Universidade Paulista (UNIP).<br />
-                <br />
-                Minha <span class="text-teal-accent-4 font-weight-medium">paixão por tecnologia vem desde criança</span>.
-                Gostava de abrir computadores, entender como funcionavam seus componentes e como funcionavam as aplicações e
-                arquivos contidos no Windows. Chegou um momento da minha vida que tive que escolher qual curso iria fazer na
-                faculdade e então optei pela <span class="text-teal-accent-4 font-weight-medium"> Ciência da
-                    Computação</span>, um curso onde abrangia de forma completa tudo o que eu precisava entender sobre
-                computação. <br />
-                <br />
-                Dentro da UNIP nós alunos tinhamos trabalhos semestrais que eram nomeados de APS, e meu primeiro trabalho em
-                grupo era criar o site completo de uma empresa fictícia onde o foco era obrigatoriamente em algo
-                ambientalmente benéfico para o mundo. Criamos então a <span
-                    class="text-teal-accent-4 font-weight-medium">AMET</span>, empresa que trabalhava com venda de tecidos
-                totalmente sustentáveis. Nosso site foi feito em <span class="text-teal-accent-4 font-weight-medium">HTML,
-                    CSS e Javascript</span> e foi um projeto muito bacana e que levaremos para toda vida. A partir desse
-                dia, eu criei uma <span class="text-teal-accent-4 font-weight-medium">paixão muito grande pelo
-                    desenvolvimento front-end</span> e fui aprimorando minhas habilidades conforme o tempo. <br />
-                <br />
-                Hoje estou numa constante evolução nos estudos e <span class="text-teal-accent-4 font-weight-medium">focado
-                    na procura do meu primeiro emprego na área de
-                    desenvolvimento</span>. Atualmente tenho habilidades em frameworks Javascript como <span
-                    class="text-teal-accent-4 font-weight-medium">Angular</span>, <span
-                    class="text-teal-accent-4 font-weight-medium">React</span> e
-                <span class="text-teal-accent-4 font-weight-medium">Vue.js</span>, além de
-                outras ferramentas como <span class="text-teal-accent-4 font-weight-medium">Git (versionamento)</span>,
-                <span class="text-teal-accent-4 font-weight-medium">Jest (testes unitários)</span>, <span
-                    class="text-teal-accent-4 font-weight-medium">Redux (gerenciamento de estado)</span>, <span
-                    class="text-teal-accent-4 font-weight-medium">Sass (pré-processador CSS)</span>, <span
-                    class="text-teal-accent-4 font-weight-medium">Tailwind CSS (framework CSS)</span>, <span
-                    class="text-teal-accent-4 font-weight-medium">Typescript (superconjunto sintático estrito de
-                    JavaScript)</span> e <span class="text-teal-accent-4 font-weight-medium">Vuetify (framework CSS)</span>.
-            </p>
-        </v-container>
-        <v-container class="d-flex flex-column" style="width: 520px;">
-            <img src="/src/assets/lucas.png" alt="Foto Lucas" />
-            <div class="w-75 d-flex justify-center items-center ga-11 py-7 px-3 mx-auto my-n7 rounded-xl mt-xl-auto"
-                style="background-color: #2b2b2b59" ref="scrollTarget">
-                <div class="d-flex flex-wrap justify-center items-center ga-12">
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/angular.png" alt="Logo Angular" />
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/react.png" alt="Logo Angular" />
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/vue.png" alt="Logo Angular" />
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/html.png" alt="Logo Angular" />
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/typescript.png"
-                        alt="Logo Angular" />
-                    <v-img :width="50" :max-width="50" :contain="true" src="/src/assets/tailwind.png" alt="Logo Angular" />
-                </div>
-            </div>
-        </v-container>
-    </v-container>
+    <v-container class="d-flex flex-column flex-md-row">
+        <v-row class="d-flex flex-column flex-lg-row justify-space-between ga-6">
+            <v-col cols="12" sm="12" md="12" lg="7" xl="6" class="d-flex flex-column ga-10 justify-center">
+                <h1 class="text-teal-accent-3 text-uppercase text-h3 font-weight-black">Sobre Mim</h1>
+                <p class="text-white text-justify font-weight-medium">
+                    Me chamo Lucas, tenho 24 anos e possuo bacharelado em 
+                    <span class="text-teal-accent-3">Ciência da Computação</span> 
+                    pela Universidade Paulista (UNIP).
+                </p>
+                <p class="text-white text-justify font-weight-medium"> 
+                    Desde criança tenho afinidade e facilidade com tecnologia, especialmente com o
+                    <span class="text-teal-accent-3">desenvolvimento front-end</span>.
+                    Na graduação, participei do projeto de uma empresa fictícia que vendia tecidos sustentáveis, usando HTML, CSS e Javascript. Atualmente estou em busca do meu primeiro emprego na área do desenvolvimento front-end. Possuo experiência em frameworks JavaScript como 
+                    <span class="text-teal-accent-3">React, Angular e Vue.js</span>,
+                    além de outras ferramentas como:
+                </p>
+                <ul class="text-white ml-4 font-weight-medium">
+                    <li>TypeScript</li>
+                    <li>Redux</li>
+                    <li>Jest</li>
+                    <li>Git</li>
+                    <li>Tailwind CSS</li>
+                    <li>Vuetify</li>
+                </ul>
+            </v-col>
+        </v-row>
 
-    <v-container class="d-flex flex-row justify-center items-center ga-16">
-
+        <v-row>
+            <v-container class="d-flex w-100 flex-column mt-n5" style="width: 520px;">
+                <img src="/src/assets/lucas.png" alt="Foto Lucas" />
+            </v-container>
+        </v-row>
     </v-container>
 </template>
   
