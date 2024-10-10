eb94627655a19ec26773b0a20b9d1689d5119c74
LucasAlNascimento <lucasalvesnascimento.lan@gmail.com>
2024-10-10 14:04:50 -0300

    Atualizando projetos

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,24 +3,24 @@
         <Header />
     </v-container>
     <v-container>
-        <div class="w-100 h-100 d-flex flex-column justify-center rounded-xl pt-5 pb-16 px-5 ga-14 "
+        <div class="w-100 h-100 d-flex flex-column justify-center rounded-xl pt-5 pb-16 px-5 ga-14"
             style="background-color: #2b2b2b59">
 
             <v-row>
-                <v-col cols="12" sm="12" md="12" lg="12" xl="12" class="w-100 d-flex flex-column flex-md-row ga-8">
+                <v-col cols="12" sm="12" md="12" lg="12" xl="12" class="w-100 d-flex flex-column flex-md-row ga-10">
                     <a href="https://luxmotors.vercel.app/" target="_blank"
                         class="d-flex justify-center">
-                        <img src="/src/assets/luxmotors.png" alt="Projeto Pokédex"
+                        <img src="/src/assets/luxmotors.png" alt="Projeto Luxmotors"
                             class="img w-100 rounded-xl justify-center" style="max-width: 500px; object-fit: contain; background-color: #E5E7EB;" />
                     </a>
-                    <div class="w-100 d-flex flex-column justify-center text-center ga-10">
+                    <div class="w-100 d-flex flex-column justify-center text-center ga-10 px-6">
                         <div class="d-flex flex-column justify-center ga-5">
                             <h2 class="text-grey-lighten-1 text-h6 font-weight-black">Luxmotors</h2>
                             <p class="text-white text-h6 font-weight-light">Projeto Front-End feito com intuito de aprender os conceitos da biblioteca React e gerenciamento de estados globais com Redux. O modelo é de uma empresa que realiza locação de carros de luxo.</p>
                         </div>
                         <div class="d-flex flex-row justify-center items-center ga-10">
                             <v-img :width="45" :max-width="45" src="/src/assets/react.png" alt="Logo React" />
-                            <v-img :width="45" :max-width="45" src="/src/assets/typescript.png" alt="Logo Typescript" />
+                            <v-img :width="45" :max-width="45" src="/src/assets/typescript.png" alt="Logo TypeScript" />
                             <v-img :width="50" :max-width="50" src="/src/assets/tailwind.png" alt="Logo Tailwind" />
                             <v-img :width="90" :max-width="90" src="/src/assets/redux.png" alt="Logo Redux" />
                         </div>
@@ -36,16 +36,39 @@
                         <img src="/src/assets/user-management-dashboard.png" alt="Projeto User Management Dashboard"
                             class="img w-100 rounded-xl justify-center" style="max-width: 500px; object-fit: contain; background-color: #fff;" />
                     </a>
-                    <div class="w-100 d-flex flex-column justify-center text-center ga-10">
+                    <div class="w-100 d-flex flex-column justify-center text-center ga-10 px-6">
                         <div class="d-flex flex-column justify-center ga-5">
-                            <h2 class="text-grey-lighten-1 text-h6 font-weight-black">User Management Dashboard</h2>
+                            <h2 class="text-white text-h6 font-weight-black">User Management Dashboard</h2>
                             <p class="text-white text-h6 font-weight-light">Projeto Front-End feito com intuito de fortalecer o conhecimento em métodos JavaScript, hooks do React e consumo de APIs com Axios. O modelo é de um painel de gerenciamento de usuários contendo paginação</p>
                         </div>
                         <div class="d-flex flex-row justify-center items-center ga-10">
                             <v-img :width="45" :max-width="45" src="/src/assets/react.png" alt="Logo React" />
-                            <v-img :width="45" :max-width="45" src="/src/assets/javascript.png" alt="Logo Typescript" />
-                            <v-img :width="90" :max-width="90" src="/src/assets/axios.svg" alt="Logo Axios" />
+                            <v-img :width="45" :max-width="45" src="/src/assets/javascript.png" alt="Logo JavaScript" />
                             <v-img :width="50" :max-width="50" src="/src/assets/tailwind.png" alt="Logo Tailwind" />
+                            <v-img :width="90" :max-width="90" src="/src/assets/axios.svg" alt="Logo Axios" />
+                        </div>
+                    </div>
+                </v-col>
+                <div class="w-100 mx-5 bg-pink-lighten-5 rounded-xl mt-15" style="height: 2px;"></div>
+            </v-row>
+
+            <v-row>
+                <v-col cols="12" sm="12" md="12" lg="12" xl="12" class="w-100 d-flex flex-column flex-md-row ga-8">
+                    <a href="https://luxmarket-project.vercel.app/" target="_blank"
+                        class="d-flex justify-center">
+                        <img src="/src/assets/luxmarket.png" alt="Projeto LUX Market"
+                            class="img w-100 rounded-xl justify-center" style="max-width: 500px; object-fit: contain; background: linear-gradient(to bottom, #0F52BA 50%, #B1B1B1 50%);" />
+                    </a>
+                    <div class="w-100 d-flex flex-column justify-center text-center ga-10 px-6">
+                        <div class="d-flex flex-column justify-center ga-5">
+                            <h2 class="text-blue-darken-3 text-h6 font-weight-black">LUX Market</h2>
+                            <p class="text-white text-h6 font-weight-light">Projeto Front-End feito com intuito de fortalecer o conhecimento em hooks do React e consumo de APIs com React Query. Desenvolvi um modelo simples de marketplace baseado em vendas de celulares utilizando a API do Mercado Livre.</p>
+                        </div>
+                        <div class="d-flex flex-row justify-center items-center ga-10">
+                            <v-img :width="45" :max-width="45" src="/src/assets/react.png" alt="Logo React" />
+                            <v-img :width="45" :max-width="45" src="/src/assets/typescript.png" alt="Logo Typescript" />
+                            <v-img :width="50" :max-width="50" src="/src/assets/tailwind.png" alt="Logo Tailwind" />
+                            <v-img :width="50" :max-width="55" src="/src/assets/react-query.png" alt="Logo React Query" />
                         </div>
                     </div>
                 </v-col>
@@ -59,7 +82,7 @@
                         <img src="/src/assets/pokedex.png" alt="Projeto Pokédex"
                             class="img w-100 rounded-xl justify-center" style="max-width: 500px; object-fit: contain; background-color: #fff;" />
                     </a>
-                    <div class="w-100 d-flex flex-column justify-center text-center ga-10">
+                    <div class="w-100 d-flex flex-column justify-center text-center ga-10 px-6">
                         <div class="d-flex flex-column justify-center ga-5">
                             <h2 class="text-pink-lighten-3 text-h6 font-weight-black">Pokédex</h2>
                             <p class="text-white text-h6 font-weight-light">Projeto Front-End feito com intuito de aprender os conceitos do framework Angular e consumo de APIs. Desenvolvi uma enciclopédia de Pokémons utilizando a PokeAPI para consumir os dados dos Pokémons.</p>
@@ -78,16 +101,16 @@
                 <v-col cols="12" sm="12" md="12" lg="12" xl="12" class="w-100 d-flex flex-column flex-md-row ga-8">
                     <a href="https://lucasheroportfolio.vercel.app/" target="_blank"
                         class="d-flex justify-center">
-                        <img src="/src/assets/portfolio.png" alt="Projeto Pokédex"
+                        <img src="/src/assets/portfolio.png" alt="Projeto Portfólio"
                             class="img w-100 rounded-xl justify-center" style="max-width: 500px; object-fit: contain; background-color: #000;" />
                     </a>
-                    <div class="w-100 d-flex flex-column justify-center text-center ga-10">
+                    <div class="w-100 d-flex flex-column justify-center text-center ga-10 px-6">
                         <div class="d-flex flex-column justify-center ga-5">
                             <h2 class="text-teal-accent-3 text-h6 font-weight-black">Portfólio</h2>
                             <p class="text-white text-h6 font-weight-light">Portfólio feito com o intuito de aprender os conceitos do framework Vue.js e, com isso, exibir meus projetos e realçar minhas realizações profissionais.</p>
                         </div>
                         <div class="d-flex flex-row justify-center items-center ga-10">
-                            <v-img :width="45" :max-width="45" src="/src/assets/vue.png" alt="Logo Angular" />
+                            <v-img :width="45" :max-width="45" src="/src/assets/vue.png" alt="Logo Vue.js" />
                             <v-img :width="45" :max-width="45" src="/src/assets/typescript.png" alt="Logo Typescript" />
                             <v-img :width="150" :max-width="150" src="/src/assets/vuetify.png" alt="Logo Vuetify" />
                         </div>
